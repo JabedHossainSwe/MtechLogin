6a793aff8483c33d17b5a1e2fd55157def37937e
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-01 01:00:58 +0600

    1 dec 23

--- index.php
+++ index.php
@@ -1,73 +1,38 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-  <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link rel="stylesheet" href="./global.css" />
+  <link rel="stylesheet" href="./index.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" />
 
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-
-  <link rel="stylesheet" href="css/index.css">
-
-  <title>Login</title>
+  <link rel="stylesheet" href="./css/index.css">
 </head>
 
 <body>
-  <div class="content">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 order-md-2">
-          <img src="images/login.png" alt="LoginImage" class="img-fluid">
-        </div>
-        <div class="col-md-6 contents">
-          <div class="row justify-content-center">
-            <div class="col-md-8">
-              <div class="mb-4">
-                <h1 style="font-size:70px; font-weight:bold;">mtech<strong
-                    style="color:red; font-size:90px;font-weight:bold;">.</strong></h1>
-                <p class="mb-4">B E Y O N D T E C H</p>
-                <p class="mb-4">Welcome to Mtech <br> Solutions</p>
-              </div>
-              <form action="#" method="post">
-                <div class="form-group first">
-                  <label for="email">Email</label>
-                  <input type="text" class="form-control" id="email">
-
-                </div>
-                <div class="form-group last mb-4">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control" id="password">
-                </div>
-
-                <div class="d-flex mb-5 align-items-center">
-                  <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                    <input type="checkbox" checked="checked" />
-                    <div class="control__indicator"></div>
-                  </label>
-                  <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span>
-                </div>
-
-                <input type="submit" value="Log In" class="btn text-white btn-block btn-primary">
-              </form>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </div>
+  <div class="login-form">
+    <div class="login-form-child"></div>
+    <b class="login">Login</b>
+    <b class="mtech">
+      <span>mtech</span>
+      <span class="span">.</span>
+    </b>
+    <b class="welcome-to-mtech-container">
+      <p class="welcome-to-mtech">Welcome to Mtech</p>
+      <p class="welcome-to-mtech">Solutions.</p>
+    </b>
+    <div class="b-e-y">B E Y O N D T E C H</div>
+    <div class="register">Register</div>
+    <b class="email">Email</b>
+    <b class="password">Password</b>
+    <div class="login-form-item"></div>
+    <div class="login-form-inner"></div>
+    <b class="remember-me">Remember me</b>
   </div>
-
-
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
+  <img src="./images/login.png" alt="">
 </body>
 
 </html>
