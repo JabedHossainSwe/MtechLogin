78dbbe5a602a3ccb320a1705556ebbd31dced016
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-01 03:10:03 +0600

    Two commits. First commit: add container div, add mtech logo, add welcome message, add login form, add register link. Second commit: make the layout responsive.

--- index.php
+++ index.php
@@ -14,48 +14,55 @@
 
 <body>
   <div class="container">
-    <div class="container-child"></div>
-    <b class="mtech">
-      <span>mtech</span>
-      <span class="span">.</span>
-    </b>
-    <b class="welcome-to-mtech-container">
-      <p class="welcome-to-mtech">Welcome to Mtech</p>
-      <p class="welcome-to-mtech">Solutions.</p>
-    </b>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="container-child">
+          <b class="mtech">
+            <span>mtech</span>
+            <span class="span">.</span>
+          </b>
+          <b class="welcome-to-mtech-container">
+            <p class="welcome-to-mtech">Welcome to Mtech</p>
+            <p class="welcome-to-mtech">Solutions.</p>
+          </b>
 
-    <div class="b-e-y">
-      B&nbsp;&nbsp;&nbsp;E&nbsp;&nbsp;&nbsp;Y&nbsp;&nbsp;&nbsp;O&nbsp;&nbsp;&nbsp;N&nbsp;&nbsp;&nbsp;D&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;T&nbsp;&nbsp;&nbsp;E&nbsp;&nbsp;&nbsp;C&nbsp;&nbsp;&nbsp;H
-    </div>
-
-    <form class="login-form" action="/login" method="post">
-      <div class="row mb-3">
-        <div class="col-md-6">
-          <label for="email" class="form-label">Email</label>
-          <input type="email" id="email" name="email" autocomplete="off" class="form-control">
-        </div> <br>
-        <div class="col-md-6">
-          <label for="password" class="form-label">Password</label>
-          <input type="password" id="password" name="password" autocomplete="off" class="form-control">
-        </div>
-      </div>
-      <div class="row mb-3">
-        <div class="col-md-6">
-          <div class="form-check">
-            <input type="checkbox" id="remember-me" name="remember-me" class="form-check-input">
-            <label for="remember-me" class="form-check-label">Remember me</label>
+          <div class="b-e-y">
+            B&nbsp;&nbsp;&nbsp;E&nbsp;&nbsp;&nbsp;Y&nbsp;&nbsp;&nbsp;O&nbsp;&nbsp;&nbsp;N&nbsp;&nbsp;&nbsp;D&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;T&nbsp;&nbsp;&nbsp;E&nbsp;&nbsp;&nbsp;C&nbsp;&nbsp;&nbsp;H
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-6 text-center">
-          <button type="submit" class="btn btn-primary btn-lg">Login</button>
-        </div>
-      </div>
-    </form>
-    <div class="register">Register</div>
 
+        <form class="login-form" action="/login" method="post">
+          <div class="row mb-3">
+            <div class="col-md-12">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" id="email" name="email" autocomplete="off" class="form-control">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-12">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" id="password" name="password" autocomplete="off" class="form-control">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-12">
+              <div class="form-check">
+                <input type="checkbox" id="remember-me" name="remember-me" class="form-check-input">
+                <label for="remember-me" class="form-check-label">Remember me</label>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <button type="submit" class="btn btn-primary btn-lg">Login</button>
+            </div>
+          </div>
+        </form>
+        <div class="register">Register</div>
+      </div>
+    </div>
   </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
