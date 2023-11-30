304b9995ca160523f232d24d18b93b697bfb4c6f
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-01 03:44:05 +0600

    Added an image to the login page and made it responsive.

--- index.php
+++ index.php
@@ -62,6 +62,9 @@
       </div>
     </div>
   </div>
+  <div class="image-container">
+    <img src="./images/login.png" alt="This is an image." class="img-fluid">
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
