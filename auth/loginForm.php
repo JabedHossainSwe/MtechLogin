0451bf4f5b447fc29ad018243cecfe8f0d600715
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-06 22:34:21 +0600

    registration & login issue solved

--- auth/loginForm.php
+++ auth/loginForm.php
@@ -1,28 +1,74 @@
-<form class="login-form" action="../auth/login_process.php" method="post">
-  <div class="row mb-3">
-    <div class="col-md-12">
-      <label for="email" class="form-label">Email</label>
-      <input type="email" id="email" name="email" autocomplete="off" class="form-control" placeholder="Email">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <div class="col-md-12">
-      <label for="password" class="form-label">Password</label>
-      <input type="password" id="password" name="password" autocomplete="off" class="form-control"
-        placeholder="Password">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <div class="col-md-12">
-      <div class="form-check">
-        <input type="checkbox" id="remember-me" name="remember-me" class="form-check-input">
-        <label for="remember-me" class="form-check-label">Remember me</label>
+<?php
+include './config/connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $password = $_POST["password"];
+
+  try {
+    $sql = "SELECT id, password FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$email]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row && password_verify($password, $row['password'])) {
+      session_start();
+      $_SESSION['user_id'] = $row['id'];
+
+      session_regenerate_id(true);
+
+      header("Location: view/dashboard.php");
+      exit(); 
+    } else {
+      
+      header("Location: index.php?error=InvalidCredentials");
+      exit();
+    }
+  } catch (PDOException $e) {
+    error_log("Error connecting to the database: " . $e->getMessage(), 0);
+    header("Location: /index.php?error=InternalServerError");
+    exit();
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <form class="login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <div class="row mb-3">
+      <div class="col-md-12">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" id="email" name="email" autocomplete="off" class="form-control" placeholder="Email">
       </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12 text-center">
-      <button type="submit" class="btn btn-primary btn-lg">Login</button>
+    <div class="row mb-3">
+      <div class="col-md-12">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" id="password" name="password" autocomplete="off" class="form-control"
+          placeholder="Password">
+      </div>
     </div>
-  </div>
-</form>
+    <div class="row mb-3">
+      <div class="col-md-12">
+        <div class="form-check">
+          <input type="checkbox" id="remember-me" name="remember-me" class="form-check-input">
+          <label for="remember-me" class="form-check-label">Remember me</label>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <button type="submit" class="btn btn-primary btn-lg">Login</button>
+      </div>
+    </div>
+  </form>
+</body>
+
+</html>
--- auth/register_process.php
+++ auth/register_process.php
@@ -2,24 +2,22 @@
 include '../config/connect.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve user input from the registration form
-    $email = $_POST["email"];
-    $password = $_POST["password"];
 
-    // Hash the password for security
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+  $email = $_POST["email"];
+  $password = $_POST["password"];
 
-    try {
-        // Prepare and execute the SQL query to insert user data into the 'users' table
-        $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([$email, $hashedPassword]);
+  $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        // Redirect to the login page upon successful registration
-        header("Location: ../index.php");
-        exit(); // Make sure to exit after the header to prevent further execution
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
+  try {
+
+    $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$email, $hashedPassword]);
+
+    header("Location: ../index.ph");
+    exit();
+  } catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+  }
 }
 ?>
