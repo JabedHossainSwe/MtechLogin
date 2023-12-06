ca992c070ea5f5d8376881065c94a6492cc2d3eb
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-06 21:56:33 +0600

    database connection successful

---
+++ config/connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$serverName = "DESKTOP-8COBBVN";
+$dbUsername = "";
+$dbPassword = "";
+$dbName = "Mtech";
+
+
+try {
+  $conn = new PDO("sqlsrv:Server=$serverName;Database=$dbName", $dbUsername, $dbPassword);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die("Error connecting to SQL Server: " . $e->getMessage());
+}
+
+
+?>
