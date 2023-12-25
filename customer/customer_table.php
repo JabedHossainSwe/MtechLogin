5439494fb7696ed5513377bd866619d8659a94c7
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-26 00:56:49 +0600

    1 am 26 december

---
+++ customer/customer_table.php
@@ -0,0 +1,21 @@
+<div class="container mt-4">
+
+  <!-- Buttons -->
+  <div class="mb-3">
+    <a href="../currency/currency.php" class="btn btn-primary mr-2">Currency</a>
+    <a href="../CustomerArea/customer_area.php" class="btn btn-primary mr-2">Area</a>
+    <a href="add_customer.php" class="btn btn-primary">Add New</a>
+  </div>
+
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>CCode</th>
+        <th>CName</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
