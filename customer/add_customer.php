c67da35371347e381bbaa4c1a5b696165d91c8d2
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-27 01:14:44 +0600

    customer adding form design done

--- customer/add_customer.php
+++ customer/add_customer.php
@@ -21,88 +21,94 @@ session_start();
 
 <body>
 
-    <div class="container mt-4">
-        <!-- Form -->
+    <div class="container mt-5">
         <form>
-            <div class="form-row align-items-center">
-                <div class="col-auto">
-                    <label for="CCode" class="mr-sm-2">Code:</label>
-                    <input type="text" class="form-control mb-2 mr-sm-2" id="CCode"
-                        value="<?php echo sprintf('%04d', mt_rand(0, 9999)); ?>" readonly>
-                </div>
-                <div class="col-auto">
-                    <label for="branch" class="mr-sm-2">Branch:</label>
-                    <input type="text" class="form-control mb-2 mr-sm-2" name="bid" id="branch">
-                </div>
-                <div class="col-auto">
-                    <label for="CName" class="mr-sm-2">Name:</label>
-                    <input type="text" class="form-control mb-2 mr-sm-2" id="CName">
+            <div class="form-row align-items-center mb-3">
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-auto">
+                            <label for="CCode" class="mr-sm-2">Code:</label>
+                            <input type="text" class="form-control mb-2 mr-sm-2" id="CCode"
+                                value="<?php echo sprintf('%04d', mt_rand(0, 9999)); ?>" readonly>
+                        </div>
+                    </div>
                 </div>
 
+                <div class="col-md-4">
+                    <div class="row align-items-end">
+                        <div class="col-md-8">
+                            <label for="bid">Branch</label>
+                            <select name="bid" id="bid" class="form-control">
+                                <option value="0">Option 0</option>
+                                <option value="1">Option 1</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="add_ref_icons d-flex justify-content-around">
+                                <button
+                                    onclick="window.open('../branch/branch.php', '_blank', 'location=yes,height=570,width=600,scrollbars=yes,status=yes');"
+                                    class="btn btn-info btn-circle mt-2" type="button">
+                                    <i class="fa fa-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <label for="CName" class="mr-sm-2">Name:</label>
+                    <input type="text" class="form-control mb-2 mr-sm-2" id="CName" value='name'>
+                </div>
+            </div>
+
+            <div class="form-row align-items-center mb-3">
                 <div class="col-md-6">
                     <label for="Description" class="mr-sm-2">Description</label>
-                    <input id="Description" value="" name="Description" type="text" class="form-control mb-2 mr-sm-2"
-                        maxlength="400">
+                    <input id="Description" value="description" name="Description" type="text"
+                        class="form-control mb-2 mr-sm-2" maxlength="400">
                 </div>
                 <div class="col-md-6">
                     <label for="Address" class="mr-sm-2">Address</label>
-                    <input id="Address" value="" name="Address" type="text" class="form-control mb-2 mr-sm-2"
+                    <input id="Address" value="address" name="Address" type="text" class="form-control mb-2 mr-sm-2"
                         maxlength="400">
                 </div>
+            </div>
 
+            <div class="form-row align-items-center mb-3">
                 <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <label for="Contact1">Phone</label>
-                            <input type="number" name="Contact1" id="Contact1" class="form-control" maxlength="200">
-                        </div>
-                    </div>
+                    <label for="Contact1">Phone</label>
+                    <input type="number" value='0' name="Contact1" id="Contact1" class="form-control" maxlength="200">
                 </div>
 
                 <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <label for="Contact2">Mobile</label>
-                            <input type="number" name="Contact2" id="Contact2" class="form-control" maxlength="200">
-                        </div>
-                    </div>
+                    <label for="Contact2">Mobile</label>
+                    <input type="number" value='0' name="Contact2" id="Contact2" class="form-control" maxlength="200">
                 </div>
 
                 <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <label for="CRNo">CR Number</label>
-                            <input type="number" name="CRNo" id="CRNo" class="form-control" maxlength="200">
-                        </div>
-                    </div>
+                    <label for="CRNo">CR Number</label>
+                    <input type="number" value='0' name="CRNo" id="CRNo" class="form-control" maxlength="200">
+                </div>
+            </div>
+
+            <div class="form-row align-items-center mb-3">
+                <div class="col-md-4">
+                    <label for="Fax">Fax</label>
+                    <input type="text" value="0" name="Fax" id="Fax" class="form-control" maxlength="200">
                 </div>
 
                 <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <label for="Fax">Fax</label>
-                            <input type="text" name="Fax" id="Fax" class="form-control" maxlength="200">
-                        </div>
-                    </div>
+                    <label for="Email">Email</label>
+                    <input type="email" name="Email" id="Email" value="test@gmail.com" class="form-control">
                 </div>
 
                 <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <label for="Email">Email</label>
-                            <input type="email" name="Email" id="Email" class="form-control">
-                        </div>
-                    </div>
+                    <label for="OpenBalance">Opening Balance</label>
+                    <input type="number" name="OpenBalance" id="OpenBalance" class="form-control" value="0">
                 </div>
+            </div>
 
-                <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <label for="OpenBalance">Opening Balance</label>
-                            <input type="number" name="OpenBalance" id="OpenBalance" class="form-control" value="0">
-                        </div>
-                    </div>
-                </div>
+            <div class="form-row align-items-center mb-3">
                 <div class="col-md-4">
                     <div class="row">
                         <div class="col-md-8">
@@ -113,34 +119,117 @@ session_start();
                     </div>
                 </div>
 
-                <div class="row align-items-end">
-                    <div class="col-md-8">
-                        <label for="custAreadiv">Customer Area</label>
-                        <select name="custAreadiv" id="custAreadiv" class="form-control">
-                            <option value="0">Option 0</option>
-                            <option value="1">Option 1</option>
-                            <!-- Add more options as needed -->
-                        </select>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="add_ref_icons d-flex justify-content-around">
-                            <button
-                                onclick="window.open('../customerArea/customer_area.php', '_blank', 'location=yes,height=570,width=600,scrollbars=yes,status=yes');"
-                                class="btn btn-info btn-circle mt-2" type="button">
-                                <i class="fa fa-plus"></i>
-                            </button>
+                <div class="col-md-4">
+                    <div class="row align-items-end">
+                        <div class="col-md-8">
+                            <label for="custAreadiv">Customer Area</label>
+                            <select name="custAreadiv" id="custAreadiv" class="form-control">
+                                <option value="0">Option 0</option>
+                                <option value="1">Option 1</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="add_ref_icons d-flex justify-content-around">
+                                <button
+                                    onclick="window.open('../customerArea/customer_area.php', '_blank', 'location=yes,height=570,width=600,scrollbars=yes,status=yes');"
+                                    class="btn btn-info btn-circle mt-2" type="button">
+                                    <i class="fa fa-plus"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-
-
+                <div class="col-md-4">
+                    <div class="row align-items-end">
+                        <div class="col-md-8">
+                            <label for="Salesman">Salesman</label>
+                            <select name="Salesman" id="Salesman" class="form-control">
+                                <option value="0">Option 0</option>
+                                <option value="1">Option 1</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="add_ref_icons d-flex justify-content-around">
+                                <button
+                                    onclick="window.open('../salesman/salesman.php', '_blank', 'location=yes,height=570,width=600,scrollbars=yes,status=yes');"
+                                    class="btn btn-info btn-circle mt-2" type="button">
+                                    <i class="fa fa-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+
+            <div class="form-row align-items-center mb-3">
+                <div class="col-md-4">
+                    <label for="StreetName" class="mr-sm-2">Street</label>
+                    <input id="StreetName" value="street" name="StreetName" type="text"
+                        class="form-control mb-2 mr-sm-2" maxlength="400">
+                </div>
+
+                <div class="col-md-4">
+                    <label for="District" class="mr-sm-2">District</label>
+                    <input id="District" value="District" name="District" type="text" class="form-control mb-2 mr-sm-2"
+                        maxlength="100">
+                </div>
+
+                <div class="col-md-4">
+                    <label for="CityN" class="mr-sm-2">City</label>
+                    <input id="CityN" value="City" name="CityN" type="text" class="form-control mb-2 mr-sm-2"
+                        maxlength="100">
+                </div>
+            </div>
+
+            <div class="form-row align-items-center mb-3">
+                <div class="col-md-4">
+                    <label for="Address" class="mr-sm-2">Country</label>
+                    <input id="Address" value="country" name="Address" type="text" class="form-control mb-2 mr-sm-2"
+                        maxlength="100">
+                </div>
+
+                <div class="col-md-4">
+                    <label for="PostalCode" class="mr-sm-2">Postal Code</label>
+                    <input id="PostalCode" value="0" name="PostalCode" type="text" class="form-control mb-2 mr-sm-2"
+                        maxlength="400">
+                </div>
+
+                <div class="col-md-4">
+                    <div class="row align-items-end">
+                        <div class="col-md-8">
+                            <label for="Currency">Currency</label>
+                            <select name="Currency" id="Currency" class="form-control">
+                                <option value="0">Option 0</option>
+                                <option value="1">Option 1</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="add_ref_icons d-flex justify-content-around">
+                                <button
+                                    onclick="window.open('../currency/currency.php', '_blank', 'location=yes,height=570,width=600,scrollbars=yes,status=yes');"
+                                    class="btn btn-info btn-circle mt-2" type="button">
+                                    <i class="fa fa-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-12">
+                <div class="row justify-content-center mt-5">
+                    <div class="col-md-3">
+                        <a href="../home.php" class="btn btn-block btn-lg btn-outline-danger">Exit</a>
+                    </div>
+
+                    <div class="col-md-3">
+                        <button type="submit" class="btn btn-block btn-lg btn-outline-success">Submit</button>
+                    </div>
+                </div>
             </div>
         </form>
-
     </div>
 
     <!-- Bootstrap JS -->
