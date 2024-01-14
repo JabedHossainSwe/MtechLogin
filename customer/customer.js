24840379ada45629535aa4af470c3f284f99f4be
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-01-15 00:32:24 +0600

    15 jan 24

--- customer/customer.js
+++ customer/customer.js
@@ -1,3 +1,24 @@
+function validateEntry() {
+  var error = 0;
+  var vvl = document.getElementsByClassName("grpreq");
+  for (var i = 0; i < vvl.length; i++) {
+    var val = $(".grpreq")[i].value;
+    var curr_Id = $(".grpreq")[i].id;
+    if (val == "") {
+      document.getElementById(curr_Id).style.border = "1px solid red";
+
+      document.getElementById(curr_Id + "_error").innerHTML =
+        " * This Field Is Required";
+      error = 1;
+    } else {
+      document.getElementById(curr_Id).style.border = "1px solid green";
+      console.log(curr_Id);
+      document.getElementById(curr_Id + "_error").innerHTML = "";
+    }
+  }
+  return error;
+}
+
 function saveCustomer() {
   var a = validateEntry();
   if (a == "1") {
